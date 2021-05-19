53bd21d65754fb94e047bb7ffb5b43784b75737a
María Fernanda Ramírez <mframirezfuentes@gmail.com>
2021-05-19 12:37:57 -0400

    Validación del campo donde se ingresa la busqueda

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
-  
+
+    $('#btnBusqueda').click(function () {
+        let id = $('#idPokemon').value;
+        let metrica1 = /^[1-9]/ + 3;
+
+        if (metrica1.test(id)) {
+
+        } else {
+            alert('Tiene que ingresar un número');
+            $('p').text('Tiene que ingresar un numero');
+        }
+    })
+
+
 });
