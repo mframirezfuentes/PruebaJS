3b2e4732ff78b14a25d72bf85d4af9ce2073990a
María Fernanda Ramírez <mframirezfuentes@gmail.com>
2021-05-20 17:19:44 -0400

    info agregada de los superheroes

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
             url: `https://www.superheroapi.com/api.php/10159429364869539/${idHeroe}/image`,
             dataType: "json",
             success: function (data) {
-                $('#imgHeroe').append(`<p>${data.url}</p>`);
+                $('#imgHeroe').append(`<img src="${data.url}" alt="100%" width="130%" class=" mt-3"></img>`);
 
             }
         });
@@ -25,9 +25,35 @@ $(document).ready(function () {
             url: `https://www.superheroapi.com/api.php/10159429364869539/${idHeroe}/powerstats`,
             dataType: "json",
             success: function (data) {
-                $('#infoHeroe').append(`<p >Nombre: ${data.name}</p>`);
-
-
+                $('#nombre').append(`<p >Nombre: ${data.name}</p>`);
+            }
+        });
+    }
+    //las conexiones del super heroe con otros super heroes
+    const conexiones=()=>{
+        id = $('#idHeroe')[0].value;
+        idHeroe = parseInt(id);
+        $.ajax({
+            type: "GET",
+            url: `https://www.superheroapi.com/api.php/10159429364869539/${idHeroe}/connections`,
+            dataType: "json",
+            success: function (data) {
+                $('#parientes').append(`<p >Parientes: ${data.relatives}</p>`);
+            }
+        });
+    }
+    const apariencia=()=>{
+        id = $('#idHeroe')[0].value;
+        idHeroe = parseInt(id);
+        $.ajax({
+            type: "GET",
+            url: `https://www.superheroapi.com/api.php/10159429364869539/${idHeroe}/appearance`,
+            dataType: "json",
+            success: function (data) {
+                $('#genero').append(`<p >Género: ${data.gender}</p>`);
+                $('#raza').append(`<p >Raza: ${data.race}</p>`);
+                $('#altura').append(`<p >Altura: ${data.height[1]}</p>`);
+                $('#peso').append(`<p >Peso: ${data.height[1]}</p>`);
             }
         });
     }
@@ -37,14 +63,16 @@ $(document).ready(function () {
         idHeroe = parseInt(id);
         let metrica1 = /^[0-9]/g;
         if (id.match(metrica1) >= 1 && id.match(metrica1) < 732) {
-            /*  let idHeroe = parseInt(id); */
+            $('#busqueda').hide();
 
             //la imagen del Heroe
-            $('#infoHeroe').append(`<p>Super Heroe Encontrado</p>`);
+            $('#imfoHeroe').append(`<p>Super Heroe Encontrado</p>`);
             //imagen del super heroe
             imgHeroe();
             //info del super heroe
             poderesHeroe();
+            conexiones();
+            apariencia();
 
         } else {
             alert('Tiene que ingresar un número, entre 1 y 731');
