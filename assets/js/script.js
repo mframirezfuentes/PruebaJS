4664788a3058e5a45401a2600dc6525bde18118e
María Fernanda Ramírez <mframirezfuentes@gmail.com>
2021-05-20 16:10:43 -0400

    se agrego la api y unas funcionalidades, como la img del super heroe y su nombre dentro de una sección

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,53 @@
 /* $('#resultados').hide(); */
 $(document).ready(function () {
+    let id = $('#idHeroe')[0].value;
+    let idHeroe = parseInt(id);
 
+    //Se va a buscar la imagen del super heroe
+    const imgHeroe = (id) => {
+        id = $('#idHeroe')[0].value;
+        idHeroe = parseInt(id);
+        $.ajax({
+            type: "GET",
+            url: `https://www.superheroapi.com/api.php/10159429364869539/${idHeroe}/image`,
+            dataType: "json",
+            success: function (data) {
+                $('#imgHeroe').append(`<p>${data.url}</p>`);
 
-    //crear lafunción flecha de lo que sucede cuando se aprieta el botón buscar.
-
-    const agregar = () => {
-        /*  $('#busqueda').hide(); */
-        
-          document.getElementById('btnBusqueda').addEventListener('click', function () {
-              $('#busqueda').hide();
-          });
-          document.getElementById('btnBusqueda').addEventListener('click', function () {
-              $('#resultados').show();
-          });
-
+            }
+        });
     }
+    const poderesHeroe = (id) => {
+        id = $('#idHeroe')[0].value;
+        idHeroe = parseInt(id);
+        $.ajax({
+            type: "GET",
+            url: `https://www.superheroapi.com/api.php/10159429364869539/${idHeroe}/powerstats`,
+            dataType: "json",
+            success: function (data) {
+                $('#infoHeroe').append(`<p >Nombre: ${data.name}</p>`);
+
+
+            }
+        });
+    }
+
     $('#btnBusqueda').click(function () {
-        let id = $('#idPokemon')[0].value;
+        id = $('#idHeroe')[0].value;
+        idHeroe = parseInt(id);
         let metrica1 = /^[0-9]/g;
-        if (id.match(metrica1)) {
+        if (id.match(metrica1) >= 1 && id.match(metrica1) < 732) {
+            /*  let idHeroe = parseInt(id); */
 
-            //llamar a la función buscar pokemon.
-            /*    buscarPokemon(); */
-            alert('ingresado correctamente');
-            agregar();
-
+            //la imagen del Heroe
+            $('#infoHeroe').append(`<p>Super Heroe Encontrado</p>`);
+            //imagen del super heroe
+            imgHeroe();
+            //info del super heroe
+            poderesHeroe();
 
         } else {
-            alert('Tiene que ingresar un número');
+            alert('Tiene que ingresar un número, entre 1 y 731');
 
         }
     });
