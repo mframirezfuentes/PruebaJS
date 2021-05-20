d797a5669444753b3f0e32c3174a6b8892e88bd2
María Fernanda Ramírez <mframirezfuentes@gmail.com>
2021-05-20 10:59:56 -0400

    Incorporando los cambios realizados

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,36 @@
+/* $('#resultados').hide(); */
 $(document).ready(function () {
 
+
+    //crear lafunción flecha de lo que sucede cuando se aprieta el botón buscar.
+
+    const agregar = () => {
+        /*  $('#busqueda').hide(); */
+        
+          document.getElementById('btnBusqueda').addEventListener('click', function () {
+              $('#busqueda').hide();
+          });
+          document.getElementById('btnBusqueda').addEventListener('click', function () {
+              $('#resultados').show();
+          });
+
+    }
     $('#btnBusqueda').click(function () {
         let id = $('#idPokemon')[0].value;
-        let metrica1 = /^[1-9]+$/;
+        let metrica1 = /^[0-9]/g;
         if (id.match(metrica1)) {
+
+            //llamar a la función buscar pokemon.
+            /*    buscarPokemon(); */
             alert('ingresado correctamente');
-            //llamar a la función
+            agregar();
+
 
         } else {
             alert('Tiene que ingresar un número');
 
         }
     });
-
-    //crear lafunción
 
 
 });
